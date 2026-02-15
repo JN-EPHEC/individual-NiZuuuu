82b7eff16cd30ead6a081e3852c98b273f1935ea
Nizuu <nicolashaulet@gmail.com>
2026-02-15 15:53:18 +0100

    TP1.5

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
+import express, { type Request, type Response } from 'express';
+
+const app= express();
+const port= 3000;
+app.get('/', (req: Request, res: Response) => {
+    res.send("Bienvenue sur mon serveur API");
+});
+app.listen(port,() => {
+    console.log('serveur lancé sur http://localhost:'+port);
+})
+
 function greet(name: string): string {
 return name+ " est le boat"
 }
