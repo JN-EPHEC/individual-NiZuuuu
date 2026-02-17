879f623ccf3537b01072156fc15768f3bcc1c63d
Nizuu <nicolashaulet@gmail.com>
2026-02-17 21:17:06 +0100

    TP1 ex6

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,16 @@
+import { Router, type Request, type Response } from 'express';
+
+const router = Router();
+
+
+const users = [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+];
+
+
+router.get('/', (req: Request, res: Response) => {
+    res.json(users);
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,30 @@
+import userRoutes from './routes/userRoutes.js';
 import express, { type Request, type Response } from 'express';
 
 const app= express();
 const port= 3000;
+app.use('/api/users', userRoutes);
+
 app.get('/', (req: Request, res: Response) => {
     res.send("Bienvenue sur mon serveur API");
 });
+
+
+//ex 6.2
+app.get('/api/hello/:name', (req, res) => {
+    
+    const name: string = req.params.name;
+    
+    
+    const now: string = new Date().toISOString();
+
+    
+    res.json({
+        message: `Bonjour ${name}`,
+        timestamp: now
+    });
+});
+
 app.listen(port,() => {
     console.log('serveur lancé sur http://localhost:'+port);
 })
