4dc73a677087ef96a9ee0f0718fb776998b1d2ca
Nicolas <nicolashaulet@gmail.com>
2026-03-16 12:53:42 +0100

    tp 3.2.2

---
+++ src/controllers/userController.ts
@@ -0,0 +1,38 @@
+import type { Request, Response } from "express";
+import User from "../models/User";
+
+// Contrôleur pour récupérer tous les utilisateurs
+export const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await User.findAll();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: (error as any).message });
+    }
+};
+
+// Contrôleur pour la création (celle avec l'email du TP précédent !)
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const { firstName, lastName, email } = req.body;
+        const newUser = await User.create({ firstName, lastName, email });
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ error: (error as any).message });
+    }
+};
+
+// Contrôleur pour la suppression
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const deleted = await User.destroy({ where: { id } });
+        if (deleted) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: "Utilisateur non trouvé" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: (error as any).message });
+    }
+};    
